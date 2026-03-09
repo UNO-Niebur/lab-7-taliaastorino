a41d533caba693daf9a422e046710c438a63ac4b
taliaastorino <tastorino@unomaha.edu>
2026-03-08 20:49:10 -0500

    Update Problem2.py
    commit

--- Problem2.py
+++ Problem2.py
@@ -1,18 +1,25 @@
 #Problem2.py
-#Project Euler problem 2
+#Project Euler problem 4
 
-from NumberTests import isEven
-from NumberTests import fibonacciSequence
+def isPalindrome(n):
+  s = str(n)
+  if s == s[::-1]:
+    return True
+  else:
+    return False
 
 def main():
-  nums = fibonacciSequence(4000001)
-  print (nums)
-  total = 0
-  for fib in nums:
-    if isEven(fib):
-      total = total + fib
+  largest = 0
+ 
+  for i in range(100, 1000):
+    for j in range(100, 1000):
+      product = i * j
+    
+      if isPalindrome(product):
+        if product > largest:
+          largest = product
   
-  print(total) # final answer
+  print(largest) # final answer
 
 if __name__ == '__main__':
   main()
