d2ee5b88433c680b0f71aab5be28b07d2beca9e5
taliaastorino <tastorino@unomaha.edu>
2026-03-08 21:02:29 -0500

    Update Problem2.py
    Heading

--- Problem2.py
+++ Problem2.py
@@ -1,5 +1,10 @@
 #Problem2.py
 #Project Euler problem 4
+#Name: Talia Astorino
+#Date: 03/08/2026
+#Purpose: Finds the largest palindrome made from the product of two 3-digit numbers.
+#The program multiplies pairs of numbers from 100 to 999 and checks if the product is a palindrome by comparing the number to its reverse.
+#Runtime is moderate because the program checks many combinations of numbers, but I optimized it by changing the range.
 
 def isPalindrome(n):
   s = str(n)
